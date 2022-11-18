7346c1e4be2d8882336e4bef3f1d0752c09d9e51
yaden-s <denisgubin@yahoo.com>
2022-11-18 18:19:28 +0400

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
