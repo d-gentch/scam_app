a5ff1b2f62def11d0d7b46ec56cc307ca19c3c05
yaden-s <denisgubin@yahoo.com>
2022-12-02 19:40:40 +0400

    Added a static page and fixed a header

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
+  serialize :avatars, JSON 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # User Avatar Validation
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
+  validates :name,  presence: true, length: { minimum: 4, maximum: 50 }
+
+  private
+    def avatar_size_validation
+      errors[:avatar] << "should be less than 5 MB!" if avatar.size > 5.megabytes
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
+
+  root            'static_pages#home'
+  get  'about' => 'static_pages#about'
+
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
 
   devise_for :users
+
+  resources  :users, only: [ :show ]
 end
